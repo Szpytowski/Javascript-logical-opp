d300697f49b05af58f3b80962866dfc192d01230
Szpytowski <renex.contact@gmail.com>
2024-10-02 17:30:31 +0200

    fixed code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 // 1
-function setAlarm(employed, vacation){
-    return employed && vacation === false;
+function setAlarm(employed, vacation) {
+    return employed && !vacation;
 }
 
 // 2
-function closeCompare(a, b, margin=0) {
-    if (a >= b - margin && a <= b + margin) {
+function closeCompare(a, b, margin = 0) {
+    if (b - margin <= a && a <= b + margin) {
         return 0;
-    } else if (a < b) {
-        return -1;
-    } else {
-        return 1;
     }
-}
+    if (a < b) {
+        return -1;
+    }
+    return 1;
 
 // 3
 const or = (a, b) => {
@@ -39,9 +38,8 @@ const xor = (a, b) => {
 function _if(bool, func1, func2) {
     if (bool) {
         return func1();
-    } else {
-        return func2();
     }
+    return func2();
 }
 
 // 5
@@ -50,8 +48,8 @@ function rentalCarCost(d) {
         return (d * 40) - 50;
     } else if (d >= 3) {
         return (d * 40) - 20;
-    } else {
-        return d * 40;
+    }
+    return d * 40;
     }
 }
 
@@ -60,18 +58,13 @@ function getRealFloor(n) {
     if (n < 0) {
         return n;
     }
-    else if (n === 0) {
-        return 0;
-    }
-    else if (n === 1) {
-        return 0;
+    else if (n === 0 || n === 1) {
+        return n;
     }
     else if (n < 13) {
         return n - 1;
     }
-    else {
-        return n - 2;
-    }
+    return n - 2;
 }
 
 // 7
@@ -87,9 +80,8 @@ function rpsls(pl1,pl2) {
         (pl1 === 'spock' && (pl2 === 'scissors' || pl2 === 'rock'))
     ) {
         return "Player 1 Won!" ;
-    } else {
-        return "Player 2 Won!";
     }
+    return "Player 2 Won!";
 }
 
 // 8
@@ -141,42 +133,58 @@ console.log(getPasswordStrength('StrongPassword123$$'));
 
 // 10
 function getTotalOrderCost(baseCost, isVIP, loyaltyPoints, isInternational) {
-    let totalCost = baseCost;
+    let totalCost = applyVIPDiscount(baseCost, isVIP);
+    totalCost = applyLoyaltyPoints(totalCost, loyaltyPoints);
+    totalCost = ensureNonNegative(totalCost);
+    let shippingCost = calculateShippingCost(isInternational);
+    return totalCost + shippingCost;
+}
 
+function applyVIPDiscount(baseCost, isVIP) {
     if (isVIP) {
-        totalCost *= 0.95;
+        return baseCost * 0.95;
     }
-    totalCost -= loyaltyPoints * 0.01;
+    return baseCost;
+}
 
+function applyLoyaltyPoints(totalCost, loyaltyPoints) {
+    return totalCost - (loyaltyPoints * 0.01);
+}
+
+function ensureNonNegative(totalCost) {
     if (totalCost < 0) {
-        totalCost = 0;
+        return 0;
     }
-
-    let shippingCost = 5;
-
-    if (isInternational) {
-        shippingCost = 10;
-    }
-
-    totalCost += shippingCost;
-
     return totalCost;
 }
+
+function calculateShippingCost(isInternational) {
+    if (isInternational) {
+        return 10;
+    }
+    return 5;
+}
+
 
 console.log(getTotalOrderCost(100, true, 100, false)); // 100*0.95 = 95 , 95-1 = 94 , 94+5 = 99
 console.log(getTotalOrderCost(100, true, 100, true)); // 100*0.95 = 95 , 95-1 = 94 , 94+10 = 104
 
 // 11
 function getTicketPrice(basePrice, daysUntilShow, isWeekend) {
+    let price = basePrice;
+
     if (daysUntilShow > 30) {
-        basePrice *= 0.90;
+        price *= 0.90;
     }
 
     if (isWeekend) {
-        basePrice += 15;
+        price += 15;
     }
 
-    return basePrice;
+    return price;
 }
 
+
 console.log(getTicketPrice(100,31,true)) // 100*0.9+15 = 105
+
+// end
